efb620340d8bd09626e9403bc35fc719e7e4c621
qefengg <qefeng@tencent.com>
2021-12-27 11:51:43 +0800

    fix: remove unused

--- src/js/lesson1/main.js
+++ src/js/lesson1/main.js
@@ -56,7 +56,6 @@ export const main = () => {
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
   gl.clearColor(0, 0, 0, 0)
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-  gl.bindVertexArray(vao)
 
   gl.useProgram(program)
   
